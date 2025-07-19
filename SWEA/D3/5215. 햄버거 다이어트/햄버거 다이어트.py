4d9c469e2bd435387243845f4f709f594b72bb62
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-07-20 01:08:46 +0900

    [D3] Title: 햄버거 다이어트, Time: 92 ms, Memory: 59,136 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -2,11 +2,8 @@ TC = int(input())
 for tc in range(1, TC + 1):
     N, L = map(int, input().split())
     items = [map(int, input().split()) for _ in range(N)]
-
     dp = [0] * (L + 1)
-
     for score, cal in items:
         for c in range(L, cal - 1, -1):
             dp[c] = max(dp[c], dp[c - cal] + score)
-
     print(f"#{tc} {max(dp)}")
