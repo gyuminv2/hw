9b128e30f21f31951e7261f4be295687d05c34b1
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-07-20 01:08:22 +0900

    [D3] Title: 햄버거 다이어트, Time: 92 ms, Memory: 59,136 KB -BaekjoonHub

--- SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
+++ SWEA/D3/5215. 햄버거 다이어트/햄버거 다이어트.py
@@ -1,17 +1,12 @@
-def dfs(i, t_score, t_kcal):
-    global m_score
-    if t_kcal > L:
-        return
-    if i == N:
-        if t_score > m_score:
-            m_score = t_score
-        return
-    dfs(i + 1, t_score + lst[i][0], t_kcal + lst[i][1])
-    dfs(i + 1, t_score, t_kcal)
 TC = int(input())
 for tc in range(1, TC + 1):
     N, L = map(int, input().split())
-    lst = [list(map(int, input().split())) for _ in range(N)]
-    m_score = 0
-    dfs(0, 0, 0)
-    print(f"#{tc} {m_score}")
+    items = [map(int, input().split()) for _ in range(N)]
+
+    dp = [0] * (L + 1)
+
+    for score, cal in items:
+        for c in range(L, cal - 1, -1):
+            dp[c] = max(dp[c], dp[c - cal] + score)
+
+    print(f"#{tc} {max(dp)}")
