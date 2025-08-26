1a661638b1a83fa44bf9354fc5a618ad709b16c9
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-08-26 14:12:20 +0900

    [D4] Title: [S/W 문제해결 응용] 4일차 - 하나로, Time: 292 ms, Memory: 83,940 KB -BaekjoonHub

--- SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
+++ SWEA/D4/1251. ［S／W 문제해결 응용］ 4일차 － 하나로/［S／W 문제해결 응용］ 4일차 － 하나로.java
@@ -4,44 +4,26 @@ import java.util.*;
 public class Solution {
 
     static int N;
-    static long[] xPos;
-    static long[] yPos;
-    static Point[] Island;
+    static Island[] islands;
     static double E;
-    static int[] parent;
+    static boolean[] v;
 
-    static class Point {
-        long i;
-        long j;
-
-        Point(long i, long j) {
-            this.i = i;
-            this.j = j;
+    static class Island {
+        long x, y;
+        Island(long x, long y) {
+            this.x = x;
+            this.y = y;
         }
     }
 
     static class Edge {
-        int from, to;
+        int to;
         double cost;
 
-        Edge(int from, int to, double cost) {
-            this.from = from;
+        Edge(int to, double cost) {
             this.to = to;
             this.cost = cost;
         }
-    }
-
-    static int find(int x) {
-        if (parent[x] == x) return x;
-        return parent[x] = find(parent[x]);
-    }
-
-    static boolean union(int a, int b) {
-        int rootA = find(a);
-        int rootB = find(b);
-        if (rootA == rootB) return false;
-        parent[rootB] = rootA;
-        return true;
     }
 
     public static void main(String[] args) throws Exception {
@@ -52,48 +34,48 @@ public class Solution {
             StringTokenizer st = new StringTokenizer(br.readLine());
 
             N = Integer.parseInt(st.nextToken());
-            xPos = new long[N];
-            yPos = new long[N];
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) xPos[i] = Long.parseLong(st.nextToken());
-            st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < N; i++) yPos[i] = Long.parseLong(st.nextToken());
-            Island = new Point[N];
-            for (int i = 0; i < N; i++) Island[i] = new Point(xPos[i], yPos[i]);
-            E = Double.parseDouble(br.readLine());
+            islands = new Island[N];
 
-            // 1. 모든 간선 정보 만들기
-            List<Edge> edgeList = new ArrayList<>();
+            StringTokenizer stX = new StringTokenizer(br.readLine());
+            StringTokenizer stY = new StringTokenizer(br.readLine());
             for (int i = 0; i < N; i++) {
-                for (int j = i + 1; j < N; j++) {
-                    long dx = Island[i].i - Island[j].i;
-                    long dy = Island[i].j - Island[j].j;
-                    double cost = E * (dx * dx + dy * dy);
-                    edgeList.add(new Edge(i, j, cost));
-                }
+                long x = Long.parseLong(stX.nextToken());
+                long y = Long.parseLong(stY.nextToken());
+                islands[i] = new Island(x, y);
             }
-
-            // 2. 간선 리스트 정렬
-            Collections.sort(edgeList, (o1, o2)-> Double.compare(o1.cost, o2.cost));
-
-            // 3. 최소 비용으로 간선 선택 (Union-Find)
-            parent = new int[N];
-            for (int i = 0; i < N; i++) parent[i] = i;
-
+            E = Double.parseDouble(br.readLine());
+            v = new boolean[N];
+            PriorityQueue<Edge> pq = new PriorityQueue<>((o1, o2) ->  Double.compare(o1.cost, o2.cost));
             double mst = 0;
-            int edgeCnt = 0;
+            int cnt = 0;
 
-            for (Edge edge : edgeList) {
-                if (union(edge.from, edge.to)) {
-                    mst += edge.cost;
-                    edgeCnt++;
-                    if (edgeCnt == N-1) break;
+            pq.offer(new Edge(0, 0.0));
+
+            while (!pq.isEmpty()) {
+                Edge cur = pq.poll();
+                int to = cur.to;
+                double cost = cur.cost;
+
+                if (v[to]) continue;
+
+                v[to] = true;
+                mst += cost;
+                cnt++;
+
+                if (cnt == N) break;
+
+                for (int i = 0; i < N; i++) {
+                    if (!v[i]) {
+                        long dx = islands[to].x - islands[i].x;
+                        long dy = islands[to].y - islands[i].y;
+                        double nCost = E * (dx*dx + dy*dy);
+                        pq.offer(new Edge(i, nCost));
+                    }
                 }
             }
 
             long ans = Math.round(mst);
             System.out.println("#" + tc + " " + ans);
-
         }
     }
 }
