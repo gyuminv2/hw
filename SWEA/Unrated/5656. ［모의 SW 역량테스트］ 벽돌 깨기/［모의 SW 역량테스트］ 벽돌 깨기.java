dabb14c34dfda7ddb6d29caef5b94536d6f8bd96
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-08-28 22:50:25 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 418 ms, Memory: 94,880 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -4,106 +4,100 @@ import java.util.*;
 public class Solution {
 
     static int N, W, H;
-    static int[][] grid;
+    static int[][] orgGrid;
     static int min;
     static int[] dis = {-1, 1, 0, 0};
     static int[] djs = {0, 0, -1, 1};
 
-    static boolean inRange(int i, int j) {
-        return i >= 0 && i < H && j >= 0 && j < W;
-    }
+    static void gravity(int[][] grid) {
+        Deque<Integer> q = new ArrayDeque<>();
 
-    static void gravity(int[][] nGrid) {
         for (int j = 0; j < W; j++) {
-            Queue<Integer> q = new LinkedList<>();
             for (int i = H-1; i >= 0; i--) {
-                if (nGrid[i][j] > 0) {
-                    q.offer(nGrid[i][j]);
-                    nGrid[i][j] = 0;
+                if (grid[i][j] > 0) {
+                    q.offer(grid[i][j]);
+                    grid[i][j] = 0;
                 }
             }
 
             int i = H-1;
             while (!q.isEmpty()) {
-                nGrid[i--][j] = q.poll();
+                grid[i--][j] = q.poll();
             }
         }
     }
 
-    static void boom(int[][] nGrid, int sj) {
-        // 벽돌찾기
-        int si = -1;
-        for (int i = 0; i < H; i++) {
-            if (nGrid[i][sj] > 0) {
-                si = i;
-                break;
-            }
-        }
-        if (si == -1) return ;
+    static boolean inRange(int i, int j) {
+        return 0 <= i && i < H && 0 <= j && j < W;
+    }
 
-        // 연쇄
+    static void boom(int[][] grid, int si, int sj) {
         Deque<int[]> q = new ArrayDeque<>();
         q.offer(new int[] {si, sj});
 
-        boolean[][] visited = new boolean[H][W];
-        visited[si][sj] = true;
-
-        List<int[]> destroy = new ArrayList<>();
-        destroy.add(new int[] {si, sj});
+        boolean[][] v = new boolean[H][W];
+        v[si][sj] = true;
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
             int ci = cur[0];
             int cj = cur[1];
-            int uranium = nGrid[ci][cj];
 
-            for (int i = 0; i < uranium; i++) {
+            for (int i = 0; i < grid[ci][cj]; i++) {
                 for (int d = 0; d < 4; d++) {
                     int ni = ci + i * dis[d];
                     int nj = cj + i * djs[d];
-                    if (inRange(ni, nj) && nGrid[ni][nj] > 0 && !visited[ni][nj]) {
-                        visited[ni][nj] = true;
+
+                    if (inRange(ni, nj) && grid[ni][nj] > 0 && !v[ni][nj]) {
+                        v[ni][nj] = true;
                         q.offer(new int[] {ni, nj});
-                        destroy.add(new int[] {ni, nj});
                     }
                 }
             }
         }
 
-//        for (int i = 0; i < H; i++) {
-//            for (int j = 0; j < W; j++) {
-//                if (visited[i][j]) nGrid[i][j] = 0;
-//            }
-//        }
-        for (int[] dest : destroy) {
-            nGrid[dest[0]][dest[1]] = 0;
+        for (int i = 0; i < H; i++) {
+            for (int j = 0 ; j < W; j++) {
+                if (v[i][j]) grid[i][j] = 0;
+            }
         }
     }
 
-    static void simul(int[][] nGrid, int[] beads) {
+    static int findTop(int[][] grid, int j) {
+        for (int i = 0; i < H; i++) {
+            if (grid[i][j] > 0) return i;
+        }
+        return -1;
+    }
+
+    static void simulate(int[][] grid, int[] beads) {
+        int si;
         for (int i = 0; i < N; i++) {
-            int sj = beads[i];
-            boom(nGrid, sj);
-            gravity(nGrid);
+            si = findTop(grid, beads[i]);
+            if (si == -1) break;
+
+            boom(grid, si, beads[i]);
+
+            gravity(grid);
         }
 
-        int remain = 0;
+        int sum = 0;
         for (int i = 0; i < H; i++) {
             for (int j = 0; j < W; j++) {
-                if (nGrid[i][j] > 0) remain++;
+                if (grid[i][j] > 0) sum++;
             }
         }
-        min = Math.min(min, remain);
+        min = Math.min(min, sum);
     }
 
     static void perm(int cnt, int[] beads) {
         if (min == 0) return;
 
         if (cnt == N) {
-            int[][] nGrid = new int[H][W];
-            for (int i = 0; i < H; i++) nGrid[i] = grid[i].clone();
-            simul(nGrid, beads);
-            return ;
+            int[][] grid = new int[H][W];
+            for (int i = 0; i < H; i++) grid[i] = orgGrid[i].clone();
+            simulate(grid, beads);
+            return;
         }
         for (int i = 0; i < W; i++) {
             beads[cnt] = i;
@@ -111,54 +105,29 @@ public class Solution {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st;
 
-        int TC = Integer.parseInt(br.readLine().trim());
-        for (int tc = 1; tc <= TC; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine().trim());
             N = Integer.parseInt(st.nextToken());
             W = Integer.parseInt(st.nextToken());
             H = Integer.parseInt(st.nextToken());
-            grid = new int[H][W];
+            orgGrid = new int[H][W];
             for (int i = 0; i < H; i++) {
-                st = new StringTokenizer(br.readLine());
+                st = new StringTokenizer(br.readLine().trim());
                 for (int j = 0; j < W; j++) {
-                    grid[i][j] = Integer.parseInt(st.nextToken());
+                    orgGrid[i][j] = Integer.parseInt(st.nextToken());
                 }
             }
             min = Integer.MAX_VALUE;
 
             int[] beads = new int[N];
-
             perm(0, beads);
 
             System.out.println("#" + tc + " " + min);
         }
     }
 }
-
-/**
- * 중복 순열로 구슬 뽑아서 (0 ~ W-1 까지의 중복순열)
- * 떨어뜨리는 시뮬
- * 연쇄 파괴 함수
- * 중력 함수
- * 블록 계산 함수
- */
-
-
-/*
-5
-3 10 10
-0 0 0 0 0 0 0 0 0 0
-1 0 1 0 1 0 0 0 0 0
-1 0 3 0 1 1 0 0 0 1
-1 1 1 0 1 2 0 0 0 9
-1 1 4 0 1 1 0 0 1 1
-1 1 4 1 1 1 2 1 1 1
-1 1 5 1 1 1 1 2 1 1
-1 1 6 1 1 1 1 1 2 1
-1 1 1 1 1 1 1 1 1 5
-1 1 7 1 1 1 1 1 1 1
- */
