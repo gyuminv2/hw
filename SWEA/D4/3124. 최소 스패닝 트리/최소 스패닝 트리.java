427851ed05102a169b88a5206ab32db132e9ba9d
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-09-01 15:33:31 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,735 ms, Memory: 120,512 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -4,61 +4,60 @@ import java.util.*;
 public class Solution {
 
     static int V, E;
-    static ArrayList<Edge> edgeLists;
+    static ArrayList<Edge> edgeList;
+    static int[] parent;
 
     static class Edge {
-        int from, to, cost;
+        int from, to;
+        long cost;
 
-        Edge(int from, int to, int cost) { this.from = from; this.to = to; this.cost = cost; }
+        Edge(int from, int to, long cost) {
+            this.from = from;
+            this.to = to;
+            this.cost = cost;
+        }
     }
 
-    static int find(int[] p, int x) {
-        if (p[x] == x) return x;
-        return p[x] = find(p, p[x]);
+    static int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x] = find(parent[x]);
     }
 
-    static boolean union(int[] p, int a, int b) {
-        int rootA = find(p, a);
-        int rootB = find(p, b);
-
+    static boolean union(int a, int b) {
+        int rootA = find(a);
+        int rootB = find(b);
         if (rootA == rootB) return false;
-        p[rootB] = rootA;
+        parent[rootB] = rootA;
         return true;
     }
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int TC = Integer.parseInt(br.readLine());
-        for (int tc = 1; tc <= TC; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
+        int T = Integer.parseInt(br.readLine());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
             V = Integer.parseInt(st.nextToken());
             E = Integer.parseInt(st.nextToken());
-            edgeLists = new ArrayList<>();
-
+            edgeList = new ArrayList<>();
             for (int i = 0; i < E; i++) {
                 st = new StringTokenizer(br.readLine());
                 int a = Integer.parseInt(st.nextToken());
                 int b = Integer.parseInt(st.nextToken());
-                int c = Integer.parseInt(st.nextToken());
-                edgeLists.add(new Edge(a, b, c));
+                long c = Long.parseLong(st.nextToken());
+                edgeList.add(new Edge(a, b, c));
             }
-            // 1. 모든 간선 정보 만들기
-            int[] parent = new int[V+1];
-            for (int i = 1; i <= V; i++) {parent[i] = i;}
-            long mst = 0;
-            int cnt = 0;
+            parent = new int[V+1];
+            for (int i = 1; i < V+1; i++) parent[i] = i;
 
-            // 2. 간선 리스트 정렬
-            Collections.sort(edgeLists, (o1, o2)->Integer.compare(o1.cost, o2.cost));
+            edgeList.sort((o1, o2) -> Long.compare(o1.cost, o2.cost));
 
-            // 3. 최소 비용으로 간선 선택 (Union-Find)
-            for (Edge e : edgeLists) {
-                if (union(parent, e.from, e.to)) {
+            long mst = 0, cnt = 0;
+            for (Edge e : edgeList) {
+                if (union(e.from, e.to)) {
                     mst += e.cost;
-                    cnt++;
-                    if (cnt == V-1) break;
+                    if (++cnt == V-1) break;
                 }
             }
 
