ef09e84b404854af0eb941adb713ad1383e0a5b2
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-09-05 12:25:46 +0900

    [Unrated] Title: [모의 SW 역량테스트] 보호 필름, Time: 708 ms, Memory: 98,532 KB -BaekjoonHub

--- SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
+++ SWEA/Unrated/2112. ［모의 SW 역량테스트］ 보호 필름/［모의 SW 역량테스트］ 보호 필름.java
@@ -1,87 +1,102 @@
 import java.io.*;
 import java.util.*;
 
+// 1. 부분집합 K개만큼
+// 2. 브루트포스
 public class Solution {
+
     static int D, W, K;
-    static int[][] film;
-    static int minCnt;
+    static int[][] grid;
+    static boolean[] isSelected;
+    static int ans;
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine().trim());
-
-        for (int tc = 1; tc <= T; tc++) {
-            StringTokenizer st = new StringTokenizer(br.readLine().trim());
-            D = Integer.parseInt(st.nextToken());
-            W = Integer.parseInt(st.nextToken());
-            K = Integer.parseInt(st.nextToken());
-            film = new int[D][W];
-            for (int i = 0; i < D; i++) {
-                st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < W; j++) {
-                    film[i][j] = Integer.parseInt(st.nextToken());
-                }
+    static int[][] copy() {
+        int[][] rtn = new int[D][W];
+        for (int i = 0; i < D; i++) {
+            for (int j = 0; j < W; j++) {
+                rtn[i][j] = grid[i][j];
             }
-            minCnt = D;
-
-            dfs(0, 0);
-
-            System.out.println("#" + tc + " " + minCnt);
         }
+        return rtn;
     }
 
-    static void dfs(int cnt, int row) {
-        if (cnt >= minCnt) {
-            return;
+    static void dfs(int[][] ngrid, boolean[] mark, int cnt, int idx) {
+        if (cnt >= ans) return ;
+
+        if (idx == D) {
+            if (check(ngrid)) ans = Math.min(ans, cnt);
+            return ;
         }
-        if (row == D) {
-            if (seqCheck()) {
-                minCnt = cnt;
-            }
-            return;
+
+        if (mark[idx]) {
+            // A
+            Arrays.fill(ngrid[idx], 0);
+            dfs(ngrid, mark, cnt+1, idx+1);
+
+            // B
+            Arrays.fill(ngrid[idx], 1);
+            dfs(ngrid, mark, cnt+1, idx+1);
         }
-        dfs(cnt, row + 1);
-        int[] orgRow = new int[W];
-        for(int i=0; i<W; i++){
-            orgRow[i] = film[row][i];
-        }
-        for (int j = 0; j < W; j++) {
-            film[row][j] = 0;
-        }
-        dfs(cnt + 1, row + 1);
-        for (int j = 0; j < W; j++) {
-            film[row][j] = 1;
-        }
-        dfs(cnt + 1, row + 1);
-        for(int i=0; i<W; i++){
-            film[row][i] = orgRow[i];
-        }
+        else dfs(ngrid, mark, cnt, idx+1);
     }
 
-    static boolean seqCheck() {
-        for (int j = 0; j < W; j++) {
-            boolean ok = false;
-            int seq = 1;
+    static void subs(int cnt) {
+        if (cnt == D) {
+            int[][] ngrid = copy();
+            dfs(ngrid, isSelected, 0, 0);
+            return ;
+        }
+        isSelected[cnt] = true;
+        subs(cnt+1);
+        isSelected[cnt] = false;
+        subs(cnt+1);
+    }
 
-            if (K == 1) {
-                ok = true;
-                continue;
-            }
+    static boolean check(int[][] g) {
+        for (int j = 0; j < W; j++) {
+            boolean pass = false;
+            int cnt = 1;
+
             for (int i = 1; i < D; i++) {
-                if (film[i][j] == film[i - 1][j]) {
-                    seq++;
-                } else {
-                    seq = 1;
-                }
-                if (seq >= K) {
-                    ok = true;
+                if (g[i][j] == g[i-1][j]) cnt++;
+                else cnt = 1;
+                if (cnt >= K) {
+                    pass = true;
                     break;
                 }
             }
-            if (!ok) {
-                return false;
-            }
+            if (!pass) return false;
         }
         return true;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int T = Integer.parseInt(st.nextToken());
+        for (int tc = 1; tc <= T; tc++) {
+            st = new StringTokenizer(br.readLine());
+            D = Integer.parseInt(st.nextToken());
+            W = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            grid = new int[D][W];
+            for (int i = 0; i < D; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < W; j++) {
+                    grid[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+            isSelected = new boolean[D];
+            ans = Integer.MAX_VALUE;
+
+            if (check(grid)) {
+                System.out.println("#"+tc+" "+0);
+            }
+            else {
+                subs(0);
+                System.out.println("#"+tc+" "+ans);
+            }
+        }
     }
 }
