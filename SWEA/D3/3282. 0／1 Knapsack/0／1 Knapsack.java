e87ed219ba4322adb381ae85849873f4919fe2c6
gyumin.park <48640323+gyuminv2@users.noreply.github.com>
2025-09-09 22:05:48 +0900

    [D3] Title: 0/1 Knapsack, Time: 124 ms, Memory: 32,496 KB -BaekjoonHub

--- SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
+++ SWEA/D3/3282. 0／1 Knapsack/0／1 Knapsack.java
@@ -4,17 +4,16 @@ import java.util.*;
 public class Solution {
 
     static int N, K;
-    static int[] V;
-    static int[] C;
-    static int[][] dp;
+    static int[] V, C;
+    static int dp[][];
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(br.readLine().trim());
+        StringTokenizer st;
 
-        int T = Integer.parseInt(st.nextToken());
         for (int tc = 1; tc <= T; tc++) {
-            st = new StringTokenizer(br.readLine());
+            st = new StringTokenizer(br.readLine().trim());
 
             N = Integer.parseInt(st.nextToken());
             K = Integer.parseInt(st.nextToken());
@@ -22,21 +21,15 @@ public class Solution {
             C = new int[N+1];
             dp = new int[N+1][K+1];
             for (int i = 1; i <= N; i++) {
-                st = new StringTokenizer(br.readLine());
+                st = new StringTokenizer(br.readLine().trim());
                 V[i] = Integer.parseInt(st.nextToken());
                 C[i] = Integer.parseInt(st.nextToken());
             }
-            for (int i = 0; i < N+1; i++) dp[i][0] = 0;
-            for (int i = 0; i < K+1; i++) dp[0][i] = 0;
 
-            for (int i = 1; i < N+1; i++) {
-                for (int j = 1; j < K+1; j++) {
-                    if (j < V[i]) {
-                        dp[i][j] = dp[i-1][j];
-                    }
-                    else {
-                        dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-V[i]] + C[i]);
-                    }
+            for (int i = 1; i <= N; i++) {
+                for (int j = 1; j <= K; j++) {
+                    if (V[i] > j) dp[i][j] = dp[i-1][j];
+                    else dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-V[i]] + C[i]);
                 }
             }
 
